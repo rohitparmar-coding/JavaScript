7d8e2aee54bd06e4a0638632187a17b7451c2bf8
Rohit Parmar <rohitparmar8349@gmail.com>
2025-08-16 02:30:59 +0000

    Container

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,41 @@
+// Data Type
+
+ "use strict"; // treat all JS code as newer version
+
+//  alert(3 + 3) //  we are using node.js not browser 
+
+// 
+console.log(3 + 8);
+console.log('3' + 8)
+console.log('3' + '8')
+console.log('3' - 8)
+console.log('3' - '8')
+console.log("Hellow")
+
+
+// 
+let name = 'Rohit'
+let age = 18
+let isLoggedIn = false
+let state = null;
+let st;
+
+// Data Type 
+// 1.Primitive dataTypes
+
+// number => 2 to power 53 
+// bigint => use for large number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => use for unique ness
+
+
+// 2.None-Primitive data type
+// 
+// Object
+console.log(typeof "parmar")
+console.log(typeof age)
+console.log(typeof null) // null o/p=> object
+console.log(typeof undefined) // undefined o/p
