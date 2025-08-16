a46022db5a47fc0b43a61177d7da6bd3ae4a0691
Rohit Parmar <rohitparmar8349@gmail.com>
2025-08-16 03:44:48 +0000

    Conversion and Operations

---
+++ 01_basic/02_conversionOperation.js
@@ -0,0 +1,58 @@
+
+//  ************** Conversion ***************** 
+
+// let score = "88jjj"
+// let score = "88"
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let isLoggedIn = 1
+//  let isLoggedIn = 0;
+// let isLoggedIn = "Parmar"
+let isLoggedIn = ''
+let booleanIsLoggIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggIn);
+
+
+let someNumber = 55
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// *************Operations *********************
+
+let value = 8
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.table([5+5 , 4-7, 7/2, 5**5, 2%8]);
+
+let str1 = "hello"
+let str2 = " Rohit"
+
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log('1' + 5)
+// console.log('2' + 2 + 2)
+// console.log('4' + 2 - 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+ "")
+
+// /let n1, n2, n3;
+
+let gameCnt = 1000
+// gameCnt++
+++gameCnt
+console.log(gameCnt)
