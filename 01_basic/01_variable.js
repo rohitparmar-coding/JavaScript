e1050297eecaf5f92d45f7083fef7680894f8e05
Rohit Parmar <rohitparmar8349@gmail.com>
2025-08-16 03:21:46 +0000

    Variable code

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "parmar@gmail.com"
+var accountPassword = '12233'
+accountCity = "Jaipur"
+let variable;
+// accountId = 2 // Not allowed
+
+accountEmail = "hellow@12.com"
+accountPassword = '111111'
+accountCity = 'Delhi'
+
+/*
+ Prefer not Var 
+*/
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, variable])
